482dc014542acb6e67b0760141fc721ff6f5a4ff
Your Name <you@example.com>
2024-12-13 23:41:45 +0330

    implement ParseTableFacade, simple interface for creating parse table

---
+++ Patterns/src/main/java/MiniJava/ParseTableFacade.java
@@ -0,0 +1,12 @@
+package MiniJava;
+
+import MiniJava.parser.ParseTable;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ParseTableFacade {
+    public static ParseTable createParseTableFromFile(String path) throws Exception {
+        return new ParseTable(Files.readAllLines(Paths.get(path)).get(0));
+    }
+}
--- Patterns/src/main/java/MiniJava/parser/Parser.java
+++ Patterns/src/main/java/MiniJava/parser/Parser.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 import MiniJava.Log.Log;
+import MiniJava.ParseTableFacade;
 import MiniJava.codeGenerator.CodeGenerator;
 import MiniJava.errorHandler.ErrorHandler;
 import MiniJava.scanner.lexicalAnalyzer;
@@ -23,7 +24,7 @@ public class Parser {
         parsStack = new Stack<Integer>();
         parsStack.push(0);
         try {
-            parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
+            parseTable = ParseTableFacade.createParseTableFromFile("src/main/resources/parseTable");
         } catch (Exception e) {
             e.printStackTrace();
         }
