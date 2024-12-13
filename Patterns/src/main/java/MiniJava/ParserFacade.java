0455ed6130d9f040f7d45bb62db62b34f559b3ad
Your Name <you@example.com>
2024-12-13 23:16:11 +0330

    implement ParserFacade, simple interface for parsing files

---
+++ Patterns/src/main/java/MiniJava/ParserFacade.java
@@ -0,0 +1,21 @@
+package MiniJava;
+
+import MiniJava.errorHandler.ErrorHandler;
+import MiniJava.parser.Parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ParserFacade {
+
+    public static void parseFile(String filePath) {
+        Parser parser = new Parser();
+        try {
+            // start parsing
+            parser.startParse(new Scanner(new File(filePath)));
+        } catch (FileNotFoundException e) {
+            ErrorHandler.printError(e.getMessage());
+        }
+    }
+}
