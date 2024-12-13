c1d589353ceda1e6f21837e0839025526a00ad63
mohammad izadi <Mohammad.Izadi@Tapsi.cab>
2024-12-14 03:00:46 +0330

    extract methods

--- Patterns/src/main/java/MiniJava/parser/ParseTable.java
+++ Patterns/src/main/java/MiniJava/parser/ParseTable.java
@@ -18,23 +18,30 @@ public class ParseTable {
     public ParseTable(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
 
-//      init Rows
         String[] Rows = jsonTable.split("],\\[");
         for (int i = 0; i < Rows.length; i++)
             Rows[i] = Rows[i].substring(1, Rows[i].length() - 1);
 
-//      init cols
         String[] cols = Rows[0].split("\",\"");
 
-//      init terminals
+        Map<Integer, Token> terminals = initTerminals(cols);
+        Map<Integer, NonTerminal> nonTerminals = initNonTerminals(cols);
+
+        initActionTable(Rows, terminals, nonTerminals);
+        initGotoTable(Rows, terminals, nonTerminals);
+    }
+
+    private static Map<Integer, Token> initTerminals(String[] cols) {
         Map<Integer, Token> terminals = new HashMap<>();
         for (int i = 1; i < cols.length; i++) {
             if (!cols[i].startsWith("Goto")) {
                 terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
             }
         }
+        return terminals;
+    }
 
-//      init nonTerminals
+    private Map<Integer, NonTerminal> initNonTerminals(String[] cols) {
         Map<Integer, NonTerminal> nonTerminals = new HashMap<>();
         for (int i = 1; i < cols.length; i++) {
             if (cols[i].startsWith("Goto")) {
@@ -46,8 +53,27 @@ public class ParseTable {
                 }
             }
         }
+        return nonTerminals;
+    }
 
-        //init action table
+    private void initGotoTable(String[] Rows, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) throws Exception {
+        for (int i = 1; i < Rows.length; i++) {
+            String[] columns = Rows[i].split("\",\"");
+
+            gotoTable.add(new HashMap<>());
+            for (int j = 1; j < columns.length; j++) {
+                if (!columns[j].isEmpty()) {
+                    if (!columns[j].equals("acc") && !terminals.containsKey(j) && nonTerminals.containsKey(j)) {
+                        gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j), Integer.parseInt(columns[j]));
+                    } else {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+    }
+
+    private void initActionTable(String[] Rows, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) throws Exception {
         for (int i = 1; i < Rows.length; i++) {
             String[] columns = Rows[i].split("\",\"");
             actionTable.add(new HashMap<>());
@@ -60,22 +86,6 @@ public class ParseTable {
                         Action a = new Action(columns[j].charAt(0) == 'r' ? act.reduce : act.shift, Integer.parseInt(columns[j].substring(1)));
                         actionTable.get(actionTable.size() - 1).put(t, a);
                     } else if (!nonTerminals.containsKey(j)) {
-                        throw new Exception();
-                    }
-                }
-            }
-        }
-
-        // init goto table
-        for (int i = 1; i < Rows.length; i++) {
-            String[] columns = Rows[i].split("\",\"");
-
-            gotoTable.add(new HashMap<>());
-            for (int j = 1; j < columns.length; j++) {
-                if (!columns[j].isEmpty()) {
-                    if (!columns[j].equals("acc") && !terminals.containsKey(j) && nonTerminals.containsKey(j)) {
-                        gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j), Integer.parseInt(columns[j]));
-                    } else {
                         throw new Exception();
                     }
                 }
